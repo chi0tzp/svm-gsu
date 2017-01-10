eb795cfac95917bc46edd46e11823d42c8d03a1f
Christos Tzelepis <tzelepis@iti.gr>
2017-01-10 09:45:33 +0200

    Add gsvm-train project

---
+++ gsvm-train/gsvm-train.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define MAX_FILENAME_LEN 4096
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+
+    /* Define filenames */
+    char mean_vectors_filename[MAX_FILENAME_LEN];
+    char covariance_matrices_filename[MAX_FILENAME_LEN];
+    char labels_filename[MAX_FILENAME_LEN];
+    char model_filename[MAX_FILENAME_LEN];
+    char obj_filename[MAX_FILENAME_LEN];
+
+
+    return 0;
+}
